8480b6556dbff6ddb2ff7f919110b88fba1eb372
dart <sumayamarikar@Sumayas-MacBook-Air.local>
2024-04-02 14:47:00 +0100

    third commit

--- capitalindexes.py
+++ capitalindexes.py
@@ -1,3 +1,12 @@
+string = input("Please input a word. I will return the index of the capital letters ")
+indexes = []
 def capitalIndexes(capitalstring):
-    print("Hello world")
+    for i in range (len(capitalstring)):
+        if ord(capitalstring[i]) >= 65 and ord(capitalstring[i])<=90: 
+            indexes.append(i)
+    return print(indexes)
+
+capitalIndexes(string)
+            
+
 
