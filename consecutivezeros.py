df03936579783ce9e6710019146a3c9b185e63db
dart <sumayamarikar@Sumayas-MacBook-Air.local>
2024-05-19 15:10:17 +0100

    third commit

--- consecutivezeros.py
+++ consecutivezeros.py
@@ -1,20 +1,22 @@
 zerostring = input("Enter a binary number")
 
 def consecutive_zeroes(binary1):
-    zerocount = 1
+    zerocount = 0
+    result = 0
     binarylength = len(binary1)
-    for i in range (binarylength):
-        if binary1[i] == "0":
-            if binary1[i] == binary1[i - 1]:
-                zerocount = zerocount + 1
-            max = zerocount
-        elif binary1[i] != "0":
-            zerocount = 1
-    if zerocount > max: 
-        print(zerocount)
-    else:
-        print(max)
-
+    for i in range (binarylength): #so the code will go into this for loop
+        if binary1[i] == "0": #when a zero is found zerocount is incrimented 
+            zerocount += 1
+        else: #if no zero count then zerocount is zero
+            zerocount = 0
+        result = max(result, zerocount) #LAST step of the for loop so it has to do this everytime. Checks whether result or zerocount is higher. 
+        #Whatever is higher becomes RESULT. So if result is 4 next time it may check whether zerocount which is 3 is higher. That way we can check the longest sequence of consecutive numbers.
+    print(result) 
+    
+        
+        
+   
+    
 consecutive_zeroes(zerostring)
 
 
