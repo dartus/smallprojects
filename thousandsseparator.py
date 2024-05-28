6cc23a6d84f92972465d446a8252e9326114b321
dart <sumayamarikar@Sumayas-MacBook-Air.local>
2024-05-28 22:54:18 +0100

    kind of works

--- thousandsseparator.py
+++ thousandsseparator.py
@@ -1,12 +1,17 @@
 num = input("Enter a large number and I will add commas to separate the thousands, enter end to stop program")
-def format_number(num):
-    while num.lower() != 'end':
-        try: 
-            int(num)
-        except ValueError:
-            print("Please enter a valid number")
-        else:
-            str(num)
-            x = num.split()
-            print(x)
+def format_number(number):
+   
+     # newnum = number[:4] + ',' + number[4:]
+     # print(newnum) 
+     #for i in range(len(number)):
+     if number[0] != '0':
+          number = number[:1] + ',' + number[1:]
+          print(number)
+          for i in range (len(number)):
+               if number[i] == ',':
+                    number = number[:i+4] + ',' + number[i+4:]
+          print(number)
+               
+      
 
+format_number(num)
